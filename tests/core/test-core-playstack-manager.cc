bfbda0fe09e02e2332179fa77902ffe8ded4dead
Hyungrok.Kim <hr97gdi@sk.com>
2020-07-07 18:22:16 +0900

    tests:apply fixture in PlayStackManager,SessionManager test
    It apply the fixture about creating instance of manager
    and sub test data in PlayStackManager and SeesionManager test.
    
    Signed-off-by: Hyungrok.Kim <hr97gdi@sk.com>

--- tests/core/test-core-playstack-manager.cc
+++ tests/core/test-core-playstack-manager.cc
@@ -54,174 +54,174 @@ static NuguDirective* createDirective(const std::string& name_space, const std::
         "ref_1", "{}", groups.c_str());
 }
 
-static void test_playstack_manager_basic(void)
-{
-    auto playstack_manager(std::make_shared<PlayStackManager>());
-    const auto& playstack_container = playstack_manager->getPlayStackContainer();
-    NuguDirective* ndir = createDirective("TTS", "test", "{ \"directives\": [\"TTS.Speak\"] }");
+typedef struct {
+    std::shared_ptr<PlayStackManager> playstack_manager;
+    std::shared_ptr<PlayStackManagerListener> playstack_manager_listener;
+    std::shared_ptr<PlayStackManagerListener> playstack_manager_listener_snd;
 
-    playstack_manager->add("", ndir);
+    NuguDirective* ndir_info;
+    NuguDirective* ndir_media;
+} TestFixture;
+
+static void setup(TestFixture* fixture, gconstpointer user_data)
+{
+    fixture->playstack_manager = std::make_shared<PlayStackManager>();
+    fixture->playstack_manager_listener = std::make_shared<PlayStackManagerListener>();
+    fixture->playstack_manager_listener_snd = std::make_shared<PlayStackManagerListener>();
+
+    fixture->ndir_info = createDirective("TTS", "test", "{ \"directives\": [\"TTS.Speak\"] }");
+    fixture->ndir_media = createDirective("AudioPlayer", "test", "{ \"directives\": [\"AudioPlayer.Play\"] }");
+}
+
+static void teardown(TestFixture* fixture, gconstpointer user_data)
+{
+    nugu_directive_unref(fixture->ndir_info);
+    nugu_directive_unref(fixture->ndir_media);
+
+    fixture->playstack_manager_listener.reset();
+    fixture->playstack_manager_listener_snd.reset();
+    fixture->playstack_manager.reset();
+}
+
+#define G_TEST_ADD_FUNC(name, func) \
+    g_test_add(name, TestFixture, nullptr, setup, func, teardown);
+
+static void test_playstack_manager_basic(TestFixture* fixture, gconstpointer ignored)
+{
+    const auto& playstack_container = fixture->playstack_manager->getPlayStackContainer();
+
+    fixture->playstack_manager->add("", fixture->ndir_info);
     g_assert(playstack_container.first.empty());
 
-    playstack_manager->add("ps_id_1", ndir);
+    fixture->playstack_manager->add("ps_id_1", fixture->ndir_info);
     g_assert(!playstack_container.first.empty());
 
     // check adding duplicately
-    playstack_manager->add("ps_id_1", ndir);
+    fixture->playstack_manager->add("ps_id_1", fixture->ndir_info);
     g_assert(!playstack_container.first.empty());
 
-    playstack_manager->remove("", PlayStackRemoveMode::Immediately);
+    fixture->playstack_manager->remove("", PlayStackRemoveMode::Immediately);
     g_assert(!playstack_container.first.empty());
 
-    playstack_manager->remove("ps_id_2", PlayStackRemoveMode::Immediately);
+    fixture->playstack_manager->remove("ps_id_2", PlayStackRemoveMode::Immediately);
     g_assert(!playstack_container.first.empty());
 
-    playstack_manager->remove("ps_id_1", PlayStackRemoveMode::Immediately);
+    fixture->playstack_manager->remove("ps_id_1", PlayStackRemoveMode::Immediately);
     g_assert(playstack_container.first.empty());
-
-    nugu_directive_unref(ndir);
 }
 
-static void test_playstack_manager_get_stack(void)
+static void test_playstack_manager_get_stack(TestFixture* fixture, gconstpointer ignored)
 {
-    auto playstack_manager(std::make_shared<PlayStackManager>());
-    NuguDirective* ndir_info = createDirective("TTS", "test", "{ \"directives\": [\"TTS.Speak\"] }");
-    NuguDirective* ndir_media = createDirective("AudioPlayer", "test", "{ \"directives\": [\"AudioPlayer.Play\"] }");
+    g_assert(fixture->playstack_manager->getAllPlayStackItems().empty());
 
-    g_assert(playstack_manager->getAllPlayStackItems().empty());
+    fixture->playstack_manager->add("ps_id_1", fixture->ndir_info);
+    g_assert(fixture->playstack_manager->getAllPlayStackItems().at(0) == "ps_id_1");
+    g_assert(fixture->playstack_manager->getPlayStackLayer("") == PlayStackLayer::None);
+    g_assert(fixture->playstack_manager->getPlayStackLayer("ps_id_1") == PlayStackLayer::Info);
 
-    playstack_manager->add("ps_id_1", ndir_info);
-    g_assert(playstack_manager->getAllPlayStackItems().at(0) == "ps_id_1");
-    g_assert(playstack_manager->getPlayStackLayer("") == PlayStackLayer::None);
-    g_assert(playstack_manager->getPlayStackLayer("ps_id_1") == PlayStackLayer::Info);
-
-    playstack_manager->remove("ps_id_1", PlayStackRemoveMode::Immediately);
-    playstack_manager->add("ps_id_2", ndir_media);
-    playstack_manager->add("ps_id_3", ndir_info);
-    auto play_stack_items = playstack_manager->getAllPlayStackItems();
+    fixture->playstack_manager->remove("ps_id_1", PlayStackRemoveMode::Immediately);
+    fixture->playstack_manager->add("ps_id_2", fixture->ndir_media);
+    fixture->playstack_manager->add("ps_id_3", fixture->ndir_info);
+    auto play_stack_items = fixture->playstack_manager->getAllPlayStackItems();
     g_assert(play_stack_items.at(0) == "ps_id_3" && play_stack_items.at(1) == "ps_id_2");
-
-    nugu_directive_unref(ndir_info);
-    nugu_directive_unref(ndir_media);
 }
 
-static void test_playstack_manager_listener(void)
+static void test_playstack_manager_listener(TestFixture* fixture, gconstpointer ignored)
 {
-    auto playstack_manager(std::make_shared<PlayStackManager>());
-    auto playstack_manager_listener(std::make_shared<PlayStackManagerListener>());
-    auto playstack_manager_listener_snd(std::make_shared<PlayStackManagerListener>());
-    NuguDirective* ndir = createDirective("TTS", "test", "{ \"directives\": [\"TTS.Speak\"] }");
+    fixture->playstack_manager->addListener(nullptr);
+    g_assert(fixture->playstack_manager->getListenerCount() == 0);
 
-    playstack_manager->addListener(nullptr);
-    g_assert(playstack_manager->getListenerCount() == 0);
-
-    playstack_manager->addListener(playstack_manager_listener.get());
-    g_assert(playstack_manager->getListenerCount() == 1);
+    fixture->playstack_manager->addListener(fixture->playstack_manager_listener.get());
+    g_assert(fixture->playstack_manager->getListenerCount() == 1);
 
     // check adding duplicately
-    playstack_manager->addListener(playstack_manager_listener.get());
-    g_assert(playstack_manager->getListenerCount() == 1);
+    fixture->playstack_manager->addListener(fixture->playstack_manager_listener.get());
+    g_assert(fixture->playstack_manager->getListenerCount() == 1);
 
-    playstack_manager->addListener(playstack_manager_listener_snd.get());
-    g_assert(playstack_manager->getListenerCount() == 2);
+    fixture->playstack_manager->addListener(fixture->playstack_manager_listener_snd.get());
+    g_assert(fixture->playstack_manager->getListenerCount() == 2);
 
-    playstack_manager->add("ps_id_1", ndir);
-    g_assert(playstack_manager_listener->getPlayServiceIds().at(0) == "ps_id_1"
-        && playstack_manager_listener_snd->getPlayServiceIds().at(0) == "ps_id_1");
+    fixture->playstack_manager->add("ps_id_1", fixture->ndir_info);
+    g_assert(fixture->playstack_manager_listener->getPlayServiceIds().at(0) == "ps_id_1"
+        && fixture->playstack_manager_listener_snd->getPlayServiceIds().at(0) == "ps_id_1");
 
-    playstack_manager->remove("ps_id_1", PlayStackRemoveMode::Immediately);
-    g_assert(playstack_manager_listener->getPlayServiceIds().empty()
-        && playstack_manager_listener_snd->getPlayServiceIds().empty());
+    fixture->playstack_manager->remove("ps_id_1", PlayStackRemoveMode::Immediately);
+    g_assert(fixture->playstack_manager_listener->getPlayServiceIds().empty()
+        && fixture->playstack_manager_listener_snd->getPlayServiceIds().empty());
 
-    playstack_manager->removeListener(nullptr);
-    g_assert(playstack_manager->getListenerCount() == 2);
+    fixture->playstack_manager->removeListener(nullptr);
+    g_assert(fixture->playstack_manager->getListenerCount() == 2);
 
-    playstack_manager->removeListener(playstack_manager_listener.get());
-    g_assert(playstack_manager->getListenerCount() == 1);
+    fixture->playstack_manager->removeListener(fixture->playstack_manager_listener.get());
+    g_assert(fixture->playstack_manager->getListenerCount() == 1);
 
-    playstack_manager->removeListener(playstack_manager_listener_snd.get());
-    g_assert(playstack_manager->getListenerCount() == 0);
-
-    nugu_directive_unref(ndir);
+    fixture->playstack_manager->removeListener(fixture->playstack_manager_listener_snd.get());
+    g_assert(fixture->playstack_manager->getListenerCount() == 0);
 }
 
-static void test_playstack_manager_holdStack(void)
+static void test_playstack_manager_holdStack(TestFixture* fixture, gconstpointer ignored)
 {
-    auto playstack_manager(std::make_shared<PlayStackManager>());
-    const auto& playstack_container = playstack_manager->getPlayStackContainer();
-    NuguDirective* ndir = createDirective("TTS", "test", "{ \"directives\": [\"TTS.Speak\"] }");
+    const auto& playstack_container = fixture->playstack_manager->getPlayStackContainer();
 
     // hold stack during 7s'
-    playstack_manager->add("ps_id_1", ndir);
-    playstack_manager->remove("ps_id_1");
+    fixture->playstack_manager->add("ps_id_1", fixture->ndir_info);
+    fixture->playstack_manager->remove("ps_id_1");
     g_assert(!playstack_container.first.empty());
 
     // remove stack immediately
-    playstack_manager->add("ps_id_2", ndir);
-    playstack_manager->remove("ps_id_2", PlayStackRemoveMode::Immediately);
+    fixture->playstack_manager->add("ps_id_2", fixture->ndir_info);
+    fixture->playstack_manager->remove("ps_id_2", PlayStackRemoveMode::Immediately);
     g_assert(playstack_container.first.empty());
 
     // hold stack during 10m'
-    playstack_manager->add("ps_id_3", ndir);
-    playstack_manager->remove("ps_id_3", PlayStackRemoveMode::Later);
+    fixture->playstack_manager->add("ps_id_3", fixture->ndir_info);
+    fixture->playstack_manager->remove("ps_id_3", PlayStackRemoveMode::Later);
     g_assert(!playstack_container.first.empty());
-
-    nugu_directive_unref(ndir);
 }
 
-static void test_playstack_manager_layerPolicy(void)
+static void test_playstack_manager_layerPolicy(TestFixture* fixture, gconstpointer ignored)
 {
-    auto playstack_manager(std::make_shared<PlayStackManager>());
-    const auto& playstack_container = playstack_manager->getPlayStackContainer();
-    NuguDirective* ndir_info = createDirective("TTS", "test", "{ \"directives\": [\"TTS.Speak\"] }");
-    NuguDirective* ndir_media = createDirective("AudioPlayer", "test", "{ \"directives\": [\"AudioPlayer.Play\"] }");
+    const auto& playstack_container = fixture->playstack_manager->getPlayStackContainer();
 
     // Info to Info -> replace
-    playstack_manager->add("ps_id_1", ndir_info);
-    playstack_manager->add("ps_id_2", ndir_info);
+    fixture->playstack_manager->add("ps_id_1", fixture->ndir_info);
+    fixture->playstack_manager->add("ps_id_2", fixture->ndir_info);
     g_assert(playstack_container.first.size() == 1
         && playstack_container.first.cbegin()->first == "ps_id_2"
         && playstack_container.first.cbegin()->second == PlayStackLayer::Info);
 
     // Info to Media -> replace
-    playstack_manager->add("ps_id_3", ndir_media);
+    fixture->playstack_manager->add("ps_id_3", fixture->ndir_media);
     g_assert(playstack_container.first.size() == 1
         && playstack_container.first.cbegin()->first == "ps_id_3"
         && playstack_container.first.cbegin()->second == PlayStackLayer::Media);
 
     // Media to Info -> stacked
-    playstack_manager->add("ps_id_4", ndir_info);
+    fixture->playstack_manager->add("ps_id_4", fixture->ndir_info);
     g_assert(playstack_container.first.size() == 2);
 
     // Media to Media -> replace
-    playstack_manager->remove("ps_id_4", PlayStackRemoveMode::Immediately);
-    playstack_manager->add("ps_id_5", ndir_media);
+    fixture->playstack_manager->remove("ps_id_4", PlayStackRemoveMode::Immediately);
+    fixture->playstack_manager->add("ps_id_5", fixture->ndir_media);
     g_assert(playstack_container.first.size() == 1
         && playstack_container.first.cbegin()->first == "ps_id_5"
         && playstack_container.first.cbegin()->second == PlayStackLayer::Media);
-
-    nugu_directive_unref(ndir_info);
-    nugu_directive_unref(ndir_media);
 }
 
-static void test_playstack_manager_controlHolding(void)
+static void test_playstack_manager_controlHolding(TestFixture* fixture, gconstpointer ignored)
 {
-    auto playstack_manager(std::make_shared<PlayStackManager>());
-    const auto& playstack_container = playstack_manager->getPlayStackContainer();
-    NuguDirective* ndir = createDirective("TTS", "test", "{ \"directives\": [\"TTS.Speak\"] }");
+    const auto& playstack_container = fixture->playstack_manager->getPlayStackContainer();
 
     // stop holding
-    playstack_manager->add("ps_id_1", ndir);
-    playstack_manager->remove("ps_id_1");
+    fixture->playstack_manager->add("ps_id_1", fixture->ndir_info);
+    fixture->playstack_manager->remove("ps_id_1");
     bool has_stack_before = !playstack_container.first.empty();
-    playstack_manager->stopHolding();
+    fixture->playstack_manager->stopHolding();
     g_assert(has_stack_before && !playstack_container.first.empty());
 
     // reset holding
-    playstack_manager->resetHolding();
-    g_assert(playstack_manager->isActiveHolding());
-
-    nugu_directive_unref(ndir);
+    fixture->playstack_manager->resetHolding();
+    g_assert(fixture->playstack_manager->isActiveHolding());
 }
 
 int main(int argc, char* argv[])
@@ -233,12 +233,12 @@ int main(int argc, char* argv[])
     g_test_init(&argc, &argv, (void*)NULL);
     g_log_set_always_fatal((GLogLevelFlags)G_LOG_FATAL_MASK);
 
-    g_test_add_func("/core/PlayStackManager/basic", test_playstack_manager_basic);
-    g_test_add_func("/core/PlayStackManager/getStack", test_playstack_manager_get_stack);
-    g_test_add_func("/core/PlayStackManager/listener", test_playstack_manager_listener);
-    g_test_add_func("/core/PlayStackManager/holdStack", test_playstack_manager_holdStack);
-    g_test_add_func("/core/PlayStackManager/layerPolicy", test_playstack_manager_layerPolicy);
-    g_test_add_func("/core/PlayStackManager/controlHolding", test_playstack_manager_controlHolding);
+    G_TEST_ADD_FUNC("/core/PlayStackManager/basic", test_playstack_manager_basic);
+    G_TEST_ADD_FUNC("/core/PlayStackManager/getStack", test_playstack_manager_get_stack);
+    G_TEST_ADD_FUNC("/core/PlayStackManager/listener", test_playstack_manager_listener);
+    G_TEST_ADD_FUNC("/core/PlayStackManager/holdStack", test_playstack_manager_holdStack);
+    G_TEST_ADD_FUNC("/core/PlayStackManager/layerPolicy", test_playstack_manager_layerPolicy);
+    G_TEST_ADD_FUNC("/core/PlayStackManager/controlHolding", test_playstack_manager_controlHolding);
 
     return g_test_run();
 }
