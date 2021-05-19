6e81f1b9a83bb9403ee9f5e726167ee7be3471e7
Hyungrok.Kim <hr97gdi@sk.com>
2021-05-19 23:31:07 +0900

    oob_server:Add OAuth2 client credentials
    It add a feature which is able to get OAuth2 token by client credentials.
    
    It remove not used element in OAuth info like pocId.
    
    It adjust UI and correct checking elements of token.
    
    Signed-off-by: Hyungrok.Kim <hr97gdi@sk.com>

--- examples/oob_setup/nugu_oob_server.py
+++ examples/oob_setup/nugu_oob_server.py
@@ -37,7 +37,6 @@ DEFAULT_JSON_AUTH = """{
 
 CONFIG_PATH_OAUTH = CONFIG_PATH + '/nugu-oauth.json'
 DEFAULT_JSON_OAUTH = """{
-    "pocId": "",
     "clientId": "",
     "clientSecret": "",
     "deviceSerialNumber": ""
@@ -60,6 +59,10 @@ def refresh_token():
     with open(CONFIG_PATH_OAUTH, 'r') as reader:
         oauthinfo = json.load(reader)
 
+    if 'refresh_token' not in authinfo or authinfo['refresh_token'] == '':
+        print '\n\033[1;93mrefresh_token is not exist\033[0m'
+        return
+
     extra = {
         'client_id': oauthinfo['clientId'],
         'client_secret': oauthinfo['clientSecret'],
@@ -77,6 +80,8 @@ def refresh_token():
 
     with open(CONFIG_PATH_AUTH, 'w') as writer:
         writer.write(token_json)
+
+    session['token'] = token
 
 @app.route('/auth', methods=['GET', 'PUT'])
 def auth():
@@ -106,26 +111,22 @@ def oauth():
         return jsonify(success=True)
 
     elif request.method == 'POST':
-        pocId = request.form['pocId']
         clientId = request.form['clientId']
         clientSecret = request.form['clientSecret']
         deviceSerialNumber = request.form['serial']
         buf = """{{
-    "pocId": "{pocId}",
     "clientId": "{clientId}",
     "clientSecret": "{clientSecret}",
     "deviceSerialNumber": "{serial}"
-}}""".format(pocId=pocId, clientId=clientId, clientSecret=clientSecret, serial=deviceSerialNumber)
+}}""".format(clientId=clientId, clientSecret=clientSecret, serial=deviceSerialNumber)
         with open(CONFIG_PATH_OAUTH, 'w') as writer:
             writer.write(buf)
         return redirect(url_for('index'))
-
 
 @app.route('/refresh', methods=['GET'])
 def refresh():
     refresh_token()
     return redirect(url_for('index'))
-
 
 @app.route('/revoke', methods=['POST'])
 def revoke():
@@ -175,12 +176,10 @@ def revoke():
     </body>
 </html>""".format(code=resp.status_code, msg=resp.text)
 
-
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
-
 
 @app.route('/')
 def index():
@@ -193,11 +192,6 @@ def index():
         issuedToken = authinfo['access_token']
     else:
         issuedToken = ''
-
-    if 'pocId' in oauthinfo:
-        pocId = oauthinfo['pocId']
-    else:
-        pocId = ''
 
     if 'clientId' in oauthinfo:
         clientId = oauthinfo['clientId']
@@ -217,10 +211,20 @@ def index():
     if 'token' in session:
         token = session['token']
         issuedToken = token['access_token']
-        if 'refresh_token' in token:
-            refresh_token=token['refresh_token']
+        if 'expires_at' in token:
+            expires_at = token['expires_at']
         else:
-            refresh_token=''
+            expires_at = ''
+        if 'refresh_token' in token:
+            refresh_token = token['refresh_token']
+            refresh_token_action = ' or <a href="/refresh">Refresh the token</a>'
+        else:
+            refresh_token = ''
+            refresh_token_action = ''
+        if 'scope' in token:
+            scope = token['scope']
+        else:
+            scope = ''
 
         loginForm = """
 <table width=100%>
@@ -250,13 +254,16 @@ def index():
     </tr>
 
 </table>
-<p align="center"><a href="/logout">Logout</a> or <a href="/refresh">Refresh the token</a></p>
-""".format(access_token=token['access_token'], expires_at=token['expires_at'], expires_in=token['expires_in'], refresh_token=refresh_token, token_type=token['token_type'], scope=token['scope'])
+<p align="center"><a href="/logout">Logout</a>{refresh_token_action}</p>
+""".format(access_token=token['access_token'], expires_at=expires_at, expires_in=token['expires_in'], refresh_token=refresh_token, token_type=token['token_type'], scope=scope, refresh_token_action=refresh_token_action)
     else:
         loginForm = """
-<p align="center"><a href="/login">Get OAuth2 token</a> or <a href="/refresh">Refresh the token</a></p>
+<p align="left">
+    <a href="/loginAuthorizationCode">Get OAuth2 token (Authorization code)</a><br/>
+    <a href="/loginClientCredentials">Get OAuth2 token (Client credentials)</a><br/>
+    <a href="/refresh">Refresh OAuth2 token</a>
+</p>
 """
-
     return """<!DOCTYPE HTML>
 <html>
     <head>
@@ -266,14 +273,11 @@ def index():
             <legend>User</legend>
             {loginForm}
         </fieldset>
+        <br/>
         <form method=post action='/oauth'>
             <fieldset>
                 <legend>OAuth2 information</legend>
                 <table width=100%>
-                    <tr>
-                        <th>poc_id</th>
-                        <td width=100%><input style="width:100%;" type=text name=pocId value="{pocId}"/></td>
-                    </tr>
                     <tr>
                         <th>client_id</th>
                         <td width=100%><input style="width:100%;" type=text name=clientId value="{clientId}"/></td>
@@ -288,12 +292,13 @@ def index():
                     </tr>
                     <tr>
                         <td colspan=2 align=center>
-                            <a href="/">Reload</a> <input type=submit value="Save"/>
+                            <button type="button" onclick="location.href='/'">Reload</button> <input type=submit value="Save"/>
                         </td>
                     </tr>
                 </table>
             </fieldset>
         </form>
+        <br/>
         <form method=post action='/revoke'>
             <fieldset>
                 <legend>Revoke</legend>
@@ -319,11 +324,10 @@ def index():
             </fieldset>
         </form>
     </body>
-</html>""".format(loginForm=loginForm, pocId=pocId, issuedToken=issuedToken, clientId=clientId, clientSecret=clientSecret, serial=serial)
+</html>""".format(loginForm=loginForm, issuedToken=issuedToken, clientId=clientId, clientSecret=clientSecret, serial=serial)
 
-
-@app.route('/login')
-def login():
+@app.route('/loginAuthorizationCode')
+def login_authorization_code():
     with open(CONFIG_PATH_OAUTH, 'r') as reader:
         oauthinfo = json.load(reader)
 
@@ -371,6 +375,39 @@ def callback():
 
     return redirect(url_for('index'))
 
+@app.route('/loginClientCredentials')
+def login_client_credentials():
+    with open(CONFIG_PATH_OAUTH, 'r') as reader:
+        oauthinfo = json.load(reader)
+
+    clientId = oauthinfo['clientId']
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': clientId,
+        'client_secret': oauthinfo['clientSecret'],
+        'data': {
+            'deviceSerialNumber': oauthinfo['deviceSerialNumber']
+        }
+    }
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': ('application/x-www-form-urlencoded;charset=UTF-8'),
+    }
+
+    nugu = OAuth2Session(clientId)
+    resp = nugu.post(token_url, data=data, headers=headers)
+    print resp
+
+    if resp.status_code == 200:
+        token_json = json.dumps(resp.json(), indent=4)
+        print token_json
+
+        with open(CONFIG_PATH_AUTH, 'w') as writer:
+            writer.write(token_json)
+
+        session['token'] = resp.json()
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     print '\033[1mNUGU Out-Of-Box sample server for authentication\033[0m'
