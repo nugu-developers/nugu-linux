ab92f9dbb86d5bbfad3a0b6b579849f2ec3a2901
Inho Oh <inho.oh@sk.com>
2021-07-20 21:59:43 +0900

    base: add request_id to manage http2 requests
    Change the HTTP2Network module to use request_id instead of a direct
    HTTP2Request pointer to manage each HTTP2Request handle.
    
    The request_id is assigned by the HTTP2Network module using the
    `http2_network_add_request()` API.
    
    Signed-off-by: Inho Oh <inho.oh@sk.com>

--- src/base/network/http2/http2_network.h
+++ src/base/network/http2/http2_network.h
@@ -29,10 +29,8 @@ HTTP2Network *http2_network_new();
 void http2_network_free(HTTP2Network *net);
 
 int http2_network_add_request(HTTP2Network *net, HTTP2Request *req);
-
-int http2_network_resume_request(HTTP2Network *net, HTTP2Request *req);
-
-int http2_network_remove_request(HTTP2Network *net, HTTP2Request *req);
+int http2_network_resume_request(HTTP2Network *net, int request_id);
+int http2_network_remove_request(HTTP2Network *net, int request_id);
 
 int http2_network_start(HTTP2Network *net);
 int http2_network_wakeup(HTTP2Network *net);
