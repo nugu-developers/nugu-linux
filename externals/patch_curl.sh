3d64949a0faff02896411330f1aa9842f78a2409
Inho Oh <inho.oh@sk.com>
2021-08-24 17:06:01 +0900

    externals: update curl to v7.79.0
    update the curl library v7.69.1 to v7.79.0
    
    Signed-off-by: Inho Oh <inho.oh@sk.com>

--- externals/patch_curl.sh
+++ externals/patch_curl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 patch -N --silent -p1 < ../0001_curl_find_nghttp2.patch
-patch -N --silent -p1 < ../0002_curl_have_poll_h.patch
+#patch -N --silent -p1 < ../0002_curl_have_poll_h.patch
 patch -N --silent -p1 < ../0003_curl_ca_variables.patch
 patch -N --silent -p1 < ../0004_curl_ca_bundle_path.patch
 
