d0cdbcd84a8f0f0c89280681ab0b0633fd9afac9
JeanTracker <hyojoong.kim.jean@gmail.com>
2022-02-03 10:48:03 +0900

    Thread-safety: NuguRunner
    NuguRunner supports thread safety by working in GMainLoop.
    
    Signed-off-by: JeanTracker <hyojoong.kim.jean@gmail.com>

---
+++ src/clientkit/nugu_runner.cc
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2019 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <glib.h>
+#include <map>
+#include <mutex>
+#include <thread>
+#include <utility>
+#include <vector>
+
+#include "base/nugu_log.h"
+#include "clientkit/nugu_runner.hh"
+
+#define DEFAULT_METHOD_TIMEOUT 500
+
+namespace NuguClientKit {
+
+class NuguRunnerPrivate {
+public:
+    NuguRunnerPrivate()
+        : done(0)
+    {
+    }
+    ~NuguRunnerPrivate()
+    {
+        tag_vector.clear();
+        type_map.clear();
+        method_map.clear();
+    }
+    static gboolean method_dispatch_cb(void* userdata);
+    void add_method(const std::string& tag, NuguRunner::request_method method, ExecuteType type);
+    void remove_method(const std::string& tag);
+
+public:
+    static std::map<NuguRunner*, NuguRunnerPrivate*> private_map;
+    std::vector<std::string> tag_vector;
+    std::map<std::string, ExecuteType> type_map;
+    std::map<std::string, NuguRunner::request_method> method_map;
+    std::mutex m;
+    std::condition_variable cv;
+    static int unique;
+    int done;
+};
+
+int NuguRunnerPrivate::unique = 0;
+std::map<NuguRunner*, NuguRunnerPrivate*> NuguRunnerPrivate::private_map;
+
+gboolean NuguRunnerPrivate::method_dispatch_cb(void* userdata)
+{
+    NuguRunner* runner = static_cast<NuguRunner*>(userdata);
+    NuguRunnerPrivate* d = NuguRunnerPrivate::private_map[runner];
+
+    if (!d) {
+        nugu_error("Nugu Runner is something wrong");
+        return FALSE;
+    }
+
+    std::string tag = d->tag_vector.front();
+    if (d->method_map.find(tag) != d->method_map.end()) {
+        nugu_info("[Method: %s] will execute", tag.c_str());
+        if (d->method_map[tag])
+            d->method_map[tag]();
+
+        ExecuteType type = d->type_map[tag];
+        d->remove_method(tag);
+
+        if (type == ExecuteType::Blocking) {
+            d->done = 1;
+            nugu_info("[Method: %s] release blocking", tag.c_str());
+            d->cv.notify_one();
+        }
+    }
+
+    return FALSE;
+}
+
+void NuguRunnerPrivate::add_method(const std::string& tag, NuguRunner::request_method method, ExecuteType type)
+{
+    std::lock_guard<std::mutex> guard(m);
+
+    nugu_info("[Method: %s] is reserved", tag.c_str());
+
+    if (std::find(tag_vector.begin(), tag_vector.end(), tag) == tag_vector.end())
+        tag_vector.push_back(tag);
+
+    method_map[tag] = std::move(method);
+    type_map[tag] = type;
+}
+
+void NuguRunnerPrivate::remove_method(const std::string& tag)
+{
+    std::lock_guard<std::mutex> guard(m);
+
+    nugu_info("[Method: %s] is removed", tag.c_str());
+
+    auto tag_iter = std::find(tag_vector.begin(), tag_vector.end(), tag);
+    if (tag_iter != tag_vector.end())
+        tag_vector.erase(tag_iter);
+
+    auto job_iter = method_map.find(tag);
+    if (job_iter != method_map.end())
+        method_map.erase(job_iter);
+
+    auto type_iter = type_map.find(tag);
+    if (type_iter != type_map.end())
+        type_map.erase(type_iter);
+}
+
+NuguRunner::NuguRunner()
+    : d(new NuguRunnerPrivate())
+{
+    d->private_map[this] = d;
+}
+
+NuguRunner::~NuguRunner()
+{
+    auto private_iter = d->private_map.find(this);
+    if (private_iter != d->private_map.end())
+        d->private_map.erase(private_iter);
+
+    delete d;
+}
+
+bool NuguRunner::invokeMethod(const std::string& tag, request_method method, ExecuteType type)
+{
+    if (type != ExecuteType::Queued) {
+        if (g_main_context_is_owner(g_main_context_default()) == TRUE) {
+            nugu_info("[Method: %s] is executed immediately", tag.c_str());
+            if (method)
+                method();
+            return true;
+        }
+    }
+
+    std::string unique_tag = tag + std::to_string(d->unique++);
+
+    d->add_method(unique_tag, std::move(method), type);
+
+    g_idle_add(d->method_dispatch_cb, (void*)this);
+    d->done = 0;
+
+    if (type == ExecuteType::Blocking) {
+        std::mutex mtx;
+        std::unique_lock<std::mutex> lk(mtx);
+
+        auto now = std::chrono::system_clock::now();
+        if (!d->cv.wait_until(lk, now + std::chrono::milliseconds(DEFAULT_METHOD_TIMEOUT),
+                [&]() { return d->done == 1; })) {
+            nugu_warn("The method(%s) is released blocking by timeout (%d msec)", unique_tag.c_str(), DEFAULT_METHOD_TIMEOUT);
+            return false;
+        }
+    }
+    return true;
+}
+
+} // NuguClientKit
---
+++ tests/clientkit/test_clientkit_nugu_runner.cc
@@ -0,0 +1,285 @@
+/*
+ * Copyright (c) 2019 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <glib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "clientkit/nugu_runner.hh"
+
+using namespace NuguClientKit;
+
+#define TIMER_1S 1
+#define TIMER_500MS 500
+#define TIMER_100MS 100
+
+class TestExecutor : public NuguRunner {
+public:
+    TestExecutor() = default;
+    ~TestExecutor() = default;
+
+    bool callBoolFunction()
+    {
+        return true;
+    }
+
+    int callIntegerFunction()
+    {
+        return 1;
+    }
+
+    std::string callStringFunction()
+    {
+        return "ok";
+    }
+
+    bool runMethod(ExecuteType type)
+    {
+        ret_bool = false;
+
+        std::string tag;
+
+        switch (type) {
+        case ExecuteType::Auto:
+            tag = __func__ + std::string("_") + "Auto";
+            break;
+        case ExecuteType::Queued:
+            tag = __func__ + std::string("_") + "Queued";
+            break;
+        case ExecuteType::Blocking:
+            tag = __func__ + std::string("_") + "Blocking";
+            break;
+        }
+
+        if (!invokeMethod(
+                tag, [&]() {
+                    ret_bool = callBoolFunction();
+                },
+                type))
+            return false;
+
+        if (type == ExecuteType::Blocking)
+            return ret_bool;
+        else
+            return true;
+    }
+
+    bool runMethodReturnBool()
+    {
+        return runMethod(ExecuteType::Blocking);
+    }
+
+    int runMethodReturnInteger()
+    {
+        ret_int = -1;
+
+        if (!invokeMethod(
+                __func__, [&]() {
+                    ret_int = callIntegerFunction();
+                },
+                ExecuteType::Blocking))
+            return -1;
+
+        return ret_int;
+    }
+
+    std::string runMethodReturnString()
+    {
+        ret_str = "";
+
+        if (!invokeMethod(
+                __func__, [&]() {
+                    ret_str = callStringFunction();
+                },
+                ExecuteType::Blocking))
+            return "";
+
+        return ret_str;
+    }
+
+    void runMethodWithSleep()
+    {
+        invokeMethod(
+            __func__, [&]() {
+                // delay for 1ms
+                usleep(1 * 1000);
+            },
+            ExecuteType::Auto);
+    }
+
+public:
+    bool ret_bool = false;
+    int ret_int = 0;
+    std::string ret_str;
+};
+
+typedef struct {
+    GMainLoop* loop;
+} nuguFixture;
+
+static void setup(nuguFixture* fixture, gconstpointer user_data)
+{
+    fixture->loop = g_main_loop_new(NULL, FALSE);
+}
+
+static void teardown(nuguFixture* fixture, gconstpointer user_data)
+{
+    g_main_loop_unref(fixture->loop);
+}
+
+#define G_TEST_ADD_FUNC(name, func) \
+    g_test_add(name, nuguFixture, NULL, setup, func, teardown);
+
+static gboolean _execute_method_runner_with_queue_on_nuguloop(gpointer userdata)
+{
+    TestExecutor test_executor;
+    GMainLoop* loop = (GMainLoop*)userdata;
+
+    g_assert(test_executor.ret_bool == false);
+    // should be executed next idle time
+    g_assert(test_executor.runMethod(ExecuteType::Queued) == true);
+    g_assert(test_executor.ret_bool == false);
+
+    // terminate loop before the job is excuted.
+    g_main_loop_quit(loop);
+    return FALSE;
+}
+
+static gboolean _execute_method_runner_with_auto_on_nuguloop(gpointer userdata)
+{
+    TestExecutor test_executor;
+    GMainLoop* loop = (GMainLoop*)userdata;
+
+    g_assert(test_executor.ret_bool == false);
+    // should be executed immediately
+    g_assert(test_executor.runMethod(ExecuteType::Auto) == true);
+    g_assert(test_executor.ret_bool == true);
+
+    // should be executed immediately
+    g_assert(test_executor.runMethod(ExecuteType::Blocking) == true);
+
+    g_main_loop_quit(loop);
+    return FALSE;
+}
+
+static void* _execute_method_runner_on_another_thread(gpointer userdata)
+{
+    TestExecutor test_executor;
+    GMainLoop* loop = (GMainLoop*)userdata;
+
+    g_assert(test_executor.runMethod(ExecuteType::Auto) == true);
+    g_assert(test_executor.runMethod(ExecuteType::Blocking) == true);
+
+    g_main_loop_quit(loop);
+    return NULL;
+}
+
+static void* _execute_method_type_runner_on_another_thread(gpointer userdata)
+{
+    TestExecutor test_executor;
+    GMainLoop* loop = (GMainLoop*)userdata;
+
+    g_assert(test_executor.runMethodReturnBool() == true);
+    g_assert(test_executor.runMethodReturnInteger() == 1);
+    g_assert(test_executor.runMethodReturnString() == "ok");
+
+    g_main_loop_quit(loop);
+    return NULL;
+}
+
+static void* _execute_method_runner_multiple_job_on_another_thread(gpointer userdata)
+{
+    TestExecutor test_executor;
+    GMainLoop* loop = (GMainLoop*)userdata;
+
+    for (int i = 0; i < 100; i++) {
+        test_executor.runMethodWithSleep();
+    }
+
+    // wait for 1sec
+    usleep(1 * 1000 * 1000);
+
+    g_main_loop_quit(loop);
+    return NULL;
+}
+
+static void test_nugu_runner_reserve_execute_method_next_idle_time_on_nuguloop(nuguFixture* fixture, gconstpointer ignored)
+{
+    GMainLoop* loop = fixture->loop;
+
+    g_timeout_add(TIMER_500MS, _execute_method_runner_with_queue_on_nuguloop, (gpointer)loop);
+
+    g_main_loop_run(loop);
+}
+
+static void test_nugu_runner_execute_method_imediately_on_nuguloop(nuguFixture* fixture, gconstpointer ignored)
+{
+    GMainLoop* loop = fixture->loop;
+
+    g_timeout_add(TIMER_500MS, _execute_method_runner_with_auto_on_nuguloop, (gpointer)loop);
+
+    g_main_loop_run(loop);
+}
+
+static void test_nugu_runner_execute_method_on_anther_thread(nuguFixture* fixture, gconstpointer ignored)
+{
+    GMainLoop* loop = fixture->loop;
+    pthread_t tid;
+
+    pthread_create(&tid, NULL, _execute_method_runner_on_another_thread, (gpointer)loop);
+
+    g_main_loop_run(loop);
+}
+
+static void test_nugu_runner_execute_method_return_type(nuguFixture* fixture, gconstpointer ignored)
+{
+    GMainLoop* loop = fixture->loop;
+    pthread_t tid;
+
+    pthread_create(&tid, NULL, _execute_method_type_runner_on_another_thread, (gpointer)loop);
+
+    g_main_loop_run(loop);
+}
+
+static void test_nugu_runner_execute_multiple_method_on_anther_thread(nuguFixture* fixture, gconstpointer ignored)
+{
+    GMainLoop* loop = fixture->loop;
+    pthread_t tid;
+
+    pthread_create(&tid, NULL, _execute_method_runner_multiple_job_on_another_thread, (gpointer)loop);
+
+    g_main_loop_run(loop);
+}
+
+int main(int argc, char* argv[])
+{
+#if !GLIB_CHECK_VERSION(2, 36, 0)
+    g_type_init();
+#endif
+
+    g_test_init(&argc, &argv, (void*)NULL);
+    g_log_set_always_fatal((GLogLevelFlags)G_LOG_FATAL_MASK);
+
+    G_TEST_ADD_FUNC("/app/NuguRunnerReserveExecuteMethodNextIdleTime", test_nugu_runner_reserve_execute_method_next_idle_time_on_nuguloop);
+    G_TEST_ADD_FUNC("/app/NuguRunnerReserveExecuteMethodImediately", test_nugu_runner_execute_method_imediately_on_nuguloop);
+    G_TEST_ADD_FUNC("/app/NuguRunnerExecuteMethodOnAnotherThread", test_nugu_runner_execute_method_on_anther_thread);
+    G_TEST_ADD_FUNC("/app/NuguRunnerExecuteMethodReturnType", test_nugu_runner_execute_method_return_type);
+    G_TEST_ADD_FUNC("/app/NuguRunnerExecuteMultipleMethodOnAnotherThread", test_nugu_runner_execute_multiple_method_on_anther_thread);
+
+    return g_test_run();
+}
