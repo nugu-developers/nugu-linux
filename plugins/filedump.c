55a9ba3f4c8f465e8e3e49205fdd411430ad69e9
JeanTracker <hyojoong.kim.jean@gmail.com>
2023-03-27 19:41:03 +0900

    plugin: file reader
    add a feature to read audio data files and perform speech recognition
    without using audio framework.
    
    Signed-off-by: JeanTracker <hyojoong.kim.jean@gmail.com>

--- plugins/filedump.c
+++ plugins/filedump.c
@@ -32,6 +32,30 @@
 static NuguDecoderDriver *decoder_driver;
 static NuguPcmDriver *pcm_driver;
 
+#ifdef NUGU_ENV_DUMP_LINK_FILE_PCM
+static void _dumpfile_link(const char *filename)
+{
+	char *link_file;
+	int ret;
+
+	if (!filename)
+		return;
+
+	link_file = getenv(NUGU_ENV_DUMP_LINK_FILE_PCM);
+	if (!link_file)
+		link_file = "tts_audio.pcm";
+
+	unlink(link_file);
+	ret = link(filename, link_file);
+	if (ret < 0) {
+		nugu_error("link(%s) failed: %s", link_file, strerror(errno));
+		return;
+	}
+
+	nugu_dbg("link file: %s -> %s", link_file, filename);
+}
+#endif
+
 static int _dumpfile_open(const char *path, const char *prefix)
 {
 	char ymd[32];
@@ -61,6 +85,9 @@ static int _dumpfile_open(const char *path, const char *prefix)
 
 	nugu_dbg("%s filedump to '%s' (fd=%d)", prefix, buf, fd);
 
+#ifdef NUGU_ENV_DUMP_LINK_FILE_PCM
+	_dumpfile_link(buf);
+#endif
 	free(buf);
 
 	return fd;
@@ -122,7 +149,7 @@ static struct nugu_decoder_driver_ops decoder_ops = {
 };
 
 static int _pcm_create(NuguPcmDriver *driver, NuguPcm *pcm,
-		     NuguAudioProperty prop)
+		       NuguAudioProperty prop)
 {
 	return 0;
 }
